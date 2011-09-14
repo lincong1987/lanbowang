ec2534bfdbbde74026e048c1e256acabd76c6ebf
lincong1987@gmail.com <lincong1987@gmail.com@93a5c658-f429-1d1e-57b9-c231f7c4806a>
2011-09-14 12:56:32 +0000

    首个版本

---
+++ trunk/index.php
@@ -0,0 +1,20 @@
+<?php include("fn/function.php");?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="description" content="中国最具执行力房地产策划机构、中国优秀商业地产实操机构、中国十佳房地产策划机构">
+<meta name="keywords" content="蓝博旺,房地产,蓝博旺不动产营运集团">
+<meta name="Author" contect="lincong1987@gmail.com">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>我明示支点，您撬动地球 - 蓝博旺不动产营运集团</title>
+<link rel="stylesheet" type="text/css" href="css/base.css"/>
+
+<style type="text/css">
+
+</style>
+</head>
+
+<body>
+
+</body>
+</html>
